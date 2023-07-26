283566858a8702a2b927326207cb5939ba070610
Xenko10 <kubinios_2002@o2.pl>
2023-07-26 09:35:23 +0200

    work page

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3000;
 
 const list = [];
+const list2 = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,9 +15,18 @@ app.get("/", (req, res) => {
   res.render("index.ejs", { text: list });
 });
 
+app.get("/work", (req, res) => {
+  res.render("work.ejs", { text: list2 });
+});
+
 app.post("/submit", (req, res) => {
   list.push(req.body.input);
   res.render("index.ejs", { text: list });
+});
+
+app.post("/submit2", (req, res) => {
+  list2.push(req.body.input);
+  res.render("work.ejs", { text: list2 });
 });
 
 app.listen(port, () => {
