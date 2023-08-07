effc2aab1ee5a0d3128d678eef18c252c0c3ed94
Xenko10 <kubinios_2002@o2.pl>
2023-08-07 10:14:03 +0200

    making it fully functional project, adding readme

--- index.js
+++ index.js
@@ -21,11 +21,35 @@ app.get("/work", (req, res) => {
 
 app.post("/submit", (req, res) => {
   list.push(req.body.input);
-  res.render("index.ejs", { text: list });
+  let arrayLength = list.length;
+  if (arrayLength === 1) {
+    res.render("index.ejs", { text: list });
+  } else if (list[arrayLength - 2] == req.body.input || req.body.input == "") {
+    list.pop();
+    res.render("index.ejs", { text: list });
+  } else {
+    res.render("index.ejs", { text: list });
+  }
 });
 
 app.post("/submit2", (req, res) => {
   list2.push(req.body.input);
+  let arrayLength = list2.length;
+  if (arrayLength === 1) {
+    res.render("work.ejs", { text: list2 });
+  } else if (list2[arrayLength - 2] == req.body.input || req.body.input == "") {
+    list2.pop();
+    res.render("work.ejs", { text: list2 });
+  } else {
+    res.render("work.ejs", { text: list2 });
+  }
+});
+
+app.get("/submit", (req, res) => {
+  res.render("index.ejs", { text: list });
+});
+
+app.get("/submit2", (req, res) => {
   res.render("work.ejs", { text: list2 });
 });
 
---
+++ readme.md
@@ -0,0 +1,7 @@
+1. Common setup
+   Clone the repo and install the dependencies.
+   - git clone https://github.com/Xenko10/todo-list-app.git
+   - npm install
+2. To start the express server, run the following
+   - node.js index.js
+   - open http://localhost:3000
